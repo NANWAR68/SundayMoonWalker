9b4351a525f35d6d9f4f06f6deceb37842a778e5
NANWAR68 <anwarlipu370@gmail.com>
2022-04-12 12:45:55 -0400

    Tuesday practice

---
+++ src/upskill/test/HashMapExercise.java
@@ -0,0 +1,24 @@
+package upskill.test;
+
+import java.util.HashMap;
+
+public class HashMapExercise {
+	
+	public static void main(String[] args){
+		
+		HashMap <Integer, String> StudentName = new HashMap <Integer, String>();
+		
+		StudentName.put(1,"Fayyaz");
+		StudentName.put(32,"Anwar");
+		StudentName.put(31,"Lipu");
+		StudentName.put(28,"Rafsan");
+		
+		System.out.println(StudentName.get(1));
+		System.out.println(StudentName.get(32));
+		System.out.println(StudentName.get(31));
+		System.out.println(StudentName.get(28));
+
+	}
+}
+
+
